154af273ae9f09a5d2bbcabd706eaae630128da2
KONSTANTINOS KOUKOS <kkonst2014@gmail.com>
2023-01-01 16:53:45 +0200

    include /dist

---
+++ dist/js/78-legacy.5963c3e7.js
@@ -0,0 +1,9 @@
+"use strict";(self["webpackChunkmusic_downloader"]=self["webpackChunkmusic_downloader"]||[]).push([[78],{4074:(e,o,t)=>{t.d(o,{a:()=>u,c:()=>d,g:()=>i,s:()=>m});var r=t(6587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const s="ION-CONTENT",n="ion-content",c=".ion-content-scroll-host",l=`${n}, ${c}`,a=e=>e.tagName===s,i=async e=>a(e)?(await new Promise((o=>(0,r.c)(e,o))),e.getScrollElement()):e,u=e=>e.closest(l),m=(e,o)=>{if(a(e)){const t=e;return t.scrollToTop(o)}return Promise.resolve(e.scrollTo({top:0,left:0,behavior:o>0?"smooth":"auto"}))},d=(e,o,t,r)=>{if(a(e)){const s=e;return s.scrollByPoint(o,t,r)}return Promise.resolve(e.scrollBy({top:t,left:o,behavior:r>0?"smooth":"auto"}))}},6078:(e,o,t)=>{t.r(o),t.d(o,{startStatusTap:()=>c});var r=t(65),s=t(4074),n=t(6587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const c=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,r.wj)((()=>{const o=e.innerWidth,t=e.innerHeight,c=document.elementFromPoint(o/2,t/2);if(!c)return;const l=(0,s.a)(c);l&&new Promise((e=>(0,n.c)(l,e))).then((()=>{(0,r.Iu)((async()=>{l.style.setProperty("--overflow","hidden"),await(0,s.s)(l,300),l.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=78-legacy.5963c3e7.js.map
