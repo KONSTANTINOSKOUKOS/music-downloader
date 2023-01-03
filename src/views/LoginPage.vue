90fb4eaaac3cf90d149e7beca28a2fe00e37c6fb
KONSTANTINOS KOUKOS <kkonst2014@gmail.com>
2023-01-03 16:07:32 +0200

    onMounted removed

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -16,49 +16,47 @@ import { IonPage, IonContent } from "@ionic/vue";
 import axios from "axios";
 import { onMounted } from "vue";
 
-onMounted(async () => {
-    console.log('LoginPage mounted');
-    const code = router.currentRoute.value.query.code;
-    if (code) {
-        console.log('code is ' + code);
-        // const res = await axios.get(`https://music-downloader-server.vercel.app/token/${code}`);
-        // state.token = res.data.token;
-        // state.refresh = res.data.refresh;
-        // state.expire = res.data.expire;
+console.log('LoginPage mounted');
+const code = router.currentRoute.value.query.code;
+if (code) {
+    console.log('code is ' + code);
+    // const res = await axios.get(`https://music-downloader-server.vercel.app/token/${code}`);
+    // state.token = res.data.token;
+    // state.refresh = res.data.refresh;
+    // state.expire = res.data.expire;
 
-        const res = await axios.post('https://accounts.spotify.com/api/token', {
-            code: code,
-            redirect_uri: 'https://music-downloader-pi.vercel.app/login',
-            grant_type: 'authorization_code'
-        }, {
-            headers: {
-                'Authorization': `Basic ${btoa(`05b24fb8ffde41c384ac3d5b54f97cf2:55eb2f966f0d401493f46a1c3c7b7ddd`)}`,
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        });
-        console.log(res.data.access_token + '\n' + res.data.refresh_token);
+    const res = await axios.post('https://accounts.spotify.com/api/token', {
+        code: code,
+        redirect_uri: 'https://music-downloader-pi.vercel.app/login',
+        grant_type: 'authorization_code'
+    }, {
+        headers: {
+            'Authorization': `Basic ${btoa(`05b24fb8ffde41c384ac3d5b54f97cf2:55eb2f966f0d401493f46a1c3c7b7ddd`)}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+    console.log(res.data.access_token + '\n' + res.data.refresh_token);
 
-        state.token = res.data.token;
-        state.refresh = res.data.refresh;
-        state.expire = res.data.expire;
+    state.token = res.data.token;
+    state.refresh = res.data.refresh;
+    state.expire = res.data.expire;
 
-        // console.log(state.token + '\n' + state.refresh);
+    // console.log(state.token + '\n' + state.refresh);
 
 
-        //CLOSURE TO ENSURE API IS FASTER THAN state.expire
-        // (function refresh() {
-        //     setTimeout(async () => {
-        //         const ress = await axios.get(`https://music-downloader-server.vercel.app/refresh/${state.refresh}`);
-        //         state.token = ress.data.token;
-        //         state.refresh = ress.data.refresh;
-        //         state.expire = ress.data.expire;
-        //         refresh();
-        //     }, state.expire * 1000 / 2);
-        // })();
+    //CLOSURE TO ENSURE API IS FASTER THAN state.expire
+    // (function refresh() {
+    //     setTimeout(async () => {
+    //         const ress = await axios.get(`https://music-downloader-server.vercel.app/refresh/${state.refresh}`);
+    //         state.token = ress.data.token;
+    //         state.refresh = ress.data.refresh;
+    //         state.expire = ress.data.expire;
+    //         refresh();
+    //     }, state.expire * 1000 / 2);
+    // })();
 
-        // router.push({ name: 'dl' });
-    }
-});
+    // router.push({ name: 'dl' });
+}
 
 const login = async () => {
     console.log('redirecting...');
