994f0f00d261282e3803a692f638a9a58ac13851
KONSTANTINOS KOUKOS <kkonst2014@gmail.com>
2023-01-05 18:34:12 +0200

    onMounted guard

--- src/views/CallbackPage.vue
+++ src/views/CallbackPage.vue
@@ -9,24 +9,25 @@ import { onMounted } from 'vue';
 onMounted(async () => {
     console.log('CallbackPage mounted');
     const code = router.currentRoute.value.query.code;
+    if (state.token != '') return;
     if (code) {
         console.log('code is ' + code);
 
-        // const res = await axios.post('https://accounts.spotify.com/api/token', {
-        //     code: code,
-        //     redirect_uri: 'https://music-downloader-pi.vercel.app/callback',
-        //     grant_type: 'authorization_code'
-        // }, {
-        //     headers: {
-        //         'Authorization': `Basic ${btoa(`05b24fb8ffde41c384ac3d5b54f97cf2:55eb2f966f0d401493f46a1c3c7b7ddd`)}`,
-        //         'Content-Type': 'application/x-www-form-urlencoded'
-        //     }
-        // });
-        // console.log(res + '\n' + res.data.access_token + '\n' + res.data.refresh_token);
+        const res = await axios.post('https://accounts.spotify.com/api/token', {
+            code: code,
+            redirect_uri: 'https://music-downloader-pi.vercel.app/callback',
+            grant_type: 'authorization_code'
+        }, {
+            headers: {
+                'Authorization': `Basic ${btoa(`05b24fb8ffde41c384ac3d5b54f97cf2:55eb2f966f0d401493f46a1c3c7b7ddd`)}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        console.log(res + '\n' + res.data.access_token + '\n' + res.data.refresh_token);
 
-        // state.token = res.data.token;
-        // state.refresh = res.data.refresh;
-        // state.expire = res.data.expire;
+        state.token = res.data.token;
+        state.refresh = res.data.refresh;
+        state.expire = res.data.expire;
     }
 });
 </script>
