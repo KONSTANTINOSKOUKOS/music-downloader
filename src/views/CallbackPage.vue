8a3d334fca8a3e26e3a8662858b5684254790c5f
KONSTANTINOS KOUKOS <kkonst2014@gmail.com>
2023-01-07 15:33:50 +0200

    PKCE implementation

--- src/views/CallbackPage.vue
+++ src/views/CallbackPage.vue
@@ -9,21 +9,22 @@ import { onMounted } from 'vue';
 onMounted(async () => {
     console.log('CallbackPage mounted');
     const code = router.currentRoute.value.query.code;
-    if (state.token != '') return;
+
     if (code) {
         console.log('code is ' + code);
 
         const res = await axios.post('https://accounts.spotify.com/api/token', {
             code: code,
             redirect_uri: 'https://music-downloader-pi.vercel.app/callback',
-            grant_type: 'authorization_code'
+            grant_type: 'authorization_code',
+            client_id: '05b24fb8ffde41c384ac3d5b54f97cf2',
+            code_verifier: localStorage.getItem('codeverifier')
         }, {
             headers: {
-                'Authorization': `Basic ${btoa(`05b24fb8ffde41c384ac3d5b54f97cf2:55eb2f966f0d401493f46a1c3c7b7ddd`)}`,
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
         });
-        console.log(res + '\n' + res.data.access_token + '\n' + res.data.refresh_token);
+        console.log(JSON.stringify(res) + '\n' + res.data.access_token + '\n' + res.data.refresh_token);
 
         localStorage.setItem('token', res.data.access_token);
         localStorage.setItem('refresh', res.data.refresh_token);
