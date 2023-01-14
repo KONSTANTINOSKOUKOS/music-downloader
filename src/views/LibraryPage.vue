331d9dc8db1cd2e984fcffaa263f32c977084c5b
KONSTANTINOS KOUKOS <kkonst2014@gmail.com>
2023-01-14 16:32:59 +0200

    imrpoving LibraryPage

--- src/views/LibraryPage.vue
+++ src/views/LibraryPage.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-content :fullscreen="true">
       <ion-row>
-        <img :src="state.user.image">
+        <img v-if="state.user.image != ''" :src="state.user.image">
         <h1>{{ state.user.name }}</h1>
       </ion-row>
       <ion-row>
@@ -13,15 +13,18 @@
       </ion-row>
       <ion-list lines="none">
         <div v-if="cat == 'tracks'">
-          <TrackComp v-for="tr in state.user.tracks" :key="tr.id" :name="tr.name" :artist="tr.artist" :id="tr.id"
+          <p v-if="state.user.tracks.length == 0">You have no saved tracks in your Spotify account!</p>
+          <TrackComp v-else v-for="tr in state.user.tracks" :key="tr.id" :name="tr.name" :artist="tr.artist" :id="tr.id"
             :image="tr.image" />
         </div>
         <div v-else-if="cat == 'playlists'">
-          <PlaylistComp @click="$router.push(`/playlist/${pl.id}`)" v-for="pl in state.user.playlists" :key="pl.id"
-            :name="pl.name" :artist="pl.owner" :id="pl.id" :image="pl.image" />
+          <p v-if="state.user.playlists.length == 0">You have no saved playlists in your Spotify account!</p>
+          <PlaylistComp v-else @click="$router.push(`/playlist/${pl.id}`)" v-for="pl in state.user.playlists"
+            :key="pl.id" :name="pl.name" :artist="pl.owner" :id="pl.id" :image="pl.image" />
         </div>
         <div v-else-if="cat == 'albums'">
-          <AlbumComp @click="$router.push(`/album/${al.id}`)" v-for="al in state.user.albums" :key="al.id"
+          <p v-if="state.user.albums.length == 0">You have no saved albums in your Spotify account!</p>
+          <AlbumComp v-else @click="$router.push(`/album/${al.id}`)" v-for="al in state.user.albums" :key="al.id"
             :name="al.name" :artist="al.artist" :id="al.id" :image="al.image" />
         </div>
       </ion-list>
